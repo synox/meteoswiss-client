eac893f8c1a39616aaf127bf3c77af50b2204c1b
synox <synox@users.noreply.github.com>
2017-05-29 21:22:35 +0200

    POC

---
+++ uri.py
@@ -0,0 +1,9 @@
+API_URL = 'http://app-prod-ws.meteoswiss.ch/v3/plzDetail2?plz=%s'
+API_OVERVIEW_URL = 'http://app-prod-ws.meteoswiss.ch/v3/plzOverview2?plz=%s'
+
+import requests
+import json
+
+url = API_URL % 8000
+resp = requests.get(url)
+data =  json.loads(resp.text)
